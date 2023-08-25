fababf8f7d3d16cdfe522a532c784ce10a11a18b
das <das@exclr8.co.za>
2023-08-25 07:06:38 +0200

    Initial Commit

---
+++ Vidya/VidyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VidyaApp.swift
+//  Vidya
+//
+//  Created by Daniel Senga on 2022/11/25.
+//
+
+import SwiftUI
+
+@main
+struct VidyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
